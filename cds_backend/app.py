32c1c05951bff24b0453c5a4ae2fc8eab3f18985
jerry23py <148231545+jerry23py@users.noreply.github.com>
2026-01-16 21:47:55 +0100

    added image upload extenstion

--- cds_backend/app.py
+++ cds_backend/app.py
@@ -463,6 +463,10 @@ def list_bank_accounts():
         "bank_type": a.bank_type
     } for a in accounts])
 
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
